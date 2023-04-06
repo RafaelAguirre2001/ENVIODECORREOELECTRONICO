e32d32e328688e9e14289fd46b056fa864a9a9f7
Jesus Rafael Arevalo Aguirre <121703302+RafaelAguirre2001@users.noreply.github.com>
2023-04-06 01:46:05 -0500

    Update corre.py

--- corre.py
+++ corre.py
@@ -6,14 +6,14 @@ from email.mime.multipart import MIMEMultipart
 
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
-smtp_username = 'programaconmigo2201022@gmail.com' #colocamos el correo electronico que utilizaremos
-smtp_password = 'kyqttzqxdgiesjzy' #aca colocamos la contraseña que generamos en la configuracion de nuestro correo .
+smtp_username = 'correo@corre.com' #colocamos el correo electronico que utilizaremos
+smtp_password = 'password' #aca colocamos la contraseña que generamos en la configuracion de nuestro correo .
 
 #creamos cuerpo del mensaje
 
 msg = MIMEMultipart()
 msg['From'] = smtp_username
-msg['To'] = 'jarevaloagu@ucvvirtual.edu.pe' #el destinatario , el correo hacia donde vamos a enviar el mensaje
+msg['To'] = 'correo destinatario@gmail.com' #el destinatario , el correo hacia donde vamos a enviar el mensaje
 msg['Subject'] = 'Prueba de mensaje' # el titulo del correo
 body = 'Hola mundo' # aca es el cuerpo del mensaje
 
